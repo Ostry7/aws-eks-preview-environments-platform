9aa6d4e91c0fa6cccd71c58d3c37aabed6580914
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-09-14 13:51:15 +0200

    add 3 priv subnets in vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Create main VPC
 resource "aws_vpc" "main-vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
   tags = {
@@ -8,31 +8,39 @@ resource "aws_vpc" "main-vpc" {
   }
 }
 
-# Create availability_zones with 3Public Subnets AZs and 3Private Subnets AZs
-# Declare the data source
+# Create availability_zones with 3Private Subnets AZs
 data "aws_availability_zones" "available" {
   state = "available"
 
 }
 
 resource "aws_subnet" "primary-priv" {
-    vpc_id = aws_vpc.main-vpc.id
-    availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.primary-priv_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
 
-  # ...
+  tags = {
+    Name = "primary-priv_subnet"
+  }
 }
 
 resource "aws_subnet" "secondary-priv" {
-    vpc_id = aws_vpc.main-vpc.id
-    availability_zone = data.aws_availability_zones.available.names[1]
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.secondary-priv_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
 
-  # ...
+  tags = {
+    Name = "secondary-priv_subnet"
+  }
 }
 
 resource "aws_subnet" "tertiary-priv" {
-    vpc_id = aws_vpc.main-vpc.id
-    availability_zone = data.aws_availability_zones.available.names[2]
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.tertiary-priv_cidr
+  availability_zone = data.aws_availability_zones.available.names[2]
 
-  # ...
+  tags = {
+    Name = "tertiary-priv_subnet"
+  }
 }
 
