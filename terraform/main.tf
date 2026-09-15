78a7bd3d673f3c7109e6a5b332c7149c54e50281
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-09-15 13:40:44 +0200

    add EKS with all dependencies

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,15 +121,76 @@ resource "aws_route_table_association" "public" {
 # Create ECR
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = "main_ecr_repo"
-  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+  image_tag_mutability = "IMMUTABLE"
+}
 
-  image_tag_mutability_exclusion_filter { #all tags are immutable except "latest*"
-    filter      = "latest*"
-    filter_type = "WILDCARD"
+# Create EKS (based on:https://developer.hashicorp.com/terraform/tutorials/kubernetes/eks)
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = var.k8s_cluster_name
+  cluster_version = "1.36"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
   }
 
-  image_tag_mutability_exclusion_filter { #all tags are immutable except "dev-*"
-    filter      = "dev-*"
-    filter_type = "WILDCARD"
+  vpc_id = aws_vpc.main-vpc.id
+  subnet_ids = [
+    aws_subnet.primary-priv.id,
+    aws_subnet.secondary-priv.id,
+    aws_subnet.tertiary-priv.id
+  ]
+  #vpc_id     = module.vpc.vpc_id
+  #subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2023_x86_64_STANDARD"
+
   }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
