c5707a4f40ec647025fcf1474fac10472c266d27
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-09-16 15:08:50 +0200

    add helm

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.6"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16"
+    }
   }
   backend "s3" {
     bucket       = "terraform-state-ostry7-2341789"
@@ -15,5 +19,17 @@ terraform {
     region       = "eu-north-1"
     encrypt      = true
     use_lockfile = true
+  }
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
