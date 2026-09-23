b96184e30cb31c89bed0fe7746be119fc99a9f7b
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-09-23 16:23:32 +0200

    adapt variable root file to use modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,37 +9,37 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "primary-priv_cidr" {
+variable "primary_priv_cidr" {
   description = "CIDR block for primary-priv"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "secondary-priv_cidr" {
+variable "secondary_priv_cidr" {
   description = "CIDR block for secondary-priv"
   type        = string
   default     = "10.0.2.0/24"
 }
 
-variable "tertiary-priv_cidr" {
+variable "tertiary_priv_cidr" {
   description = "CIDR block for tertiary-priv"
   type        = string
   default     = "10.0.3.0/24"
 }
 
-variable "primary-pub_cidr" {
+variable "primary_pub_cidr" {
   description = "CIDR block for primary-pub"
   type        = string
   default     = "10.0.101.0/24"
 }
 
-variable "secondary-pub_cidr" {
+variable "secondary_pub_cidr" {
   description = "CIDR block for secondary-pub"
   type        = string
   default     = "10.0.102.0/24"
 }
 
-variable "tertiary-pub_cidr" {
+variable "tertiary_pub_cidr" {
   description = "CIDR block for tertiary-pub"
   type        = string
   default     = "10.0.103.0/24"
