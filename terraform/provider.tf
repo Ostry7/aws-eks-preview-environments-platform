3f16aea3b409c562fd4baa8463205a15f5240f8c
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-09-23 16:23:20 +0200

    adapt provider root file to use modules

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -30,8 +30,7 @@ provider "aws" {
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
   depends_on = [
-    aws_eks_access_entry.ci_role,
-    aws_eks_access_policy_association.ci_role_admin,
+    module.eks
   ]
 }
 
